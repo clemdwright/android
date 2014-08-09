0c1acd376388a22f354c348e62f6f0d8a1ae9d26
Clem Wright <clemw@google.com>
2014-08-09 14:12:13 -0700

    first commit

---
+++ src/com/google/android/apps/tastebuds/PlaceDetails.java
@@ -0,0 +1,13 @@
+package com.google.android.apps.tastebuds;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PlaceDetails extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place_details);
+    }
+}
